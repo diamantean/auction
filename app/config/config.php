e5cb9fab39e8feb82a028e4422cf986e738f3a9a
diamantean <102007463+diamantean@users.noreply.github.com>
2023-10-14 12:56:44 +0700

    Update config.php

--- app/config/config.php
+++ app/config/config.php
@@ -1,7 +1,7 @@
 <?php
 
 const APP_NAME = 'Auction';
-const BASE_URL = 'http://localhost/lelang';
+const BASE_URL = 'http://aucibid.vercel.app';
 const DB_HOST = 'localhost';
 const DB_USER = 'root';
 const DB_PASS = '';
